aef35932595f4e05d8e60d6715931276ba34b5bc
henriqtorresl <henriquetlandin@gmail.com>
2024-03-06 10:49:38 -0300

    configuração do swagger para o vercel

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,4 @@ app.listen(3000, () => {
     console.log('Rodando em "http://localhost:3000"');
 });
 
-
-const CSS_URL = " https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css ";
-
-swaggerUi.setup({ customCssUrl: CSS_URL })
-
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
