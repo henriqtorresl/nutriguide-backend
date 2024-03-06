775ebca179ca8002cf73937c8c0bf0d0544b74fb
henriqtorresl <henriquetlandin@gmail.com>
2024-03-06 13:04:31 -0300

    config pro heroku

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,14 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './documentation/swagger.json'
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Rodando em "http://localhost:3000"');
 });
 
