ad049624108a5ccb9f4c8894b1642d640db0fc28
henriqtorresl <henriquetlandin@gmail.com>
2024-03-07 11:23:11 -0300

    configurações no swagger

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,34 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs, 
+    {
+        customCss:
+          `
+            .swagger-ui .opblock .opblock-summary-path-description-wrapper { 
+                align-items: center; 
+                display: flex; 
+                flex-wrap: wrap; 
+                gap: 0 10px; 
+                padding: 0 10px; 
+                width: 100%; 
+            }
+            .opblock-control-arrow {
+                border: none !important;
+                background: none !important;
+            }
+            .schemes-server-container {
+                select {
+                    text-transform: uppercase !important; 
+                    margin-left: 15px;
+                    cursor: pointer;
+                }
+            }
+          `,
+        customCssUrl: CSS_URL,
+    }    
+));
 
 app.listen(port, () => {
     console.log(`Rodando em "http://localhost:${port}"`);
